95f76ea1bad3170304abead4c3c5139c869d2419
Sayantan Sen <icesanu.2019@gmail.com>
2024-12-26 17:45:50 +0530

    Project Files

--- app/src/main/java/com/example/breeze/MainActivity.kt
+++ app/src/main/java/com/example/breeze/MainActivity.kt
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         firebaseAuth = FirebaseAuth.getInstance()
