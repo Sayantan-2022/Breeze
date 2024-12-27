0ce98275af96d00c790292842145c8b1bcc7bf25
Sayantan Sen <icesanu.2019@gmail.com>
2024-12-27 21:57:42 +0530

    Project Files

--- app/src/main/java/com/example/breeze/MainActivity.kt
+++ app/src/main/java/com/example/breeze/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.example.breeze
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.breeze.ui.bookmarks.BookmarksFragment
 import com.example.breeze.ui.home.HomeFragment
 import com.example.breeze.ui.search.SearchFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -29,6 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val window : Window = window
+        window.statusBarColor = getColor(R.color.dark_blue)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
